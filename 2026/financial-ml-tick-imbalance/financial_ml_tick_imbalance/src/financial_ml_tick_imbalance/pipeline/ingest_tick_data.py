0bb57195a3384ede3166fe42b7b8baa7314202fa
raj <rroy271111@gmail.com>
2026-04-10 12:39:32 +0530

    fix: Missing imports and syntax error

--- 2026/financial-ml-tick-imbalance/financial_ml_tick_imbalance/src/financial_ml_tick_imbalance/pipeline/ingest_tick_data.py
+++ 2026/financial-ml-tick-imbalance/financial_ml_tick_imbalance/src/financial_ml_tick_imbalance/pipeline/ingest_tick_data.py
@@ -1,16 +1,22 @@
-from financial_ml_tick_imbalance.data.dukascopy_loader import(download_tick_data,)
+from financial_ml_tick_imbalance.data.dukascopy_loader import (
+    download_tick_data,
+)
+
+from datetime import datetime
+
 
 def run_ingestion():
     df = download_tick_data(
         symbol="EURUSD",
-        start=datetime(2026,1,1)
-        end=datetime(2026,1,3)      
+        start=datetime(2026, 1, 1),
+        end=datetime(2026, 1, 3),
     )
 
     df.to_csv(
-        "data/raw/dukascopy/EURUSD_2026_01",
-        index=False,    
+        "data/raw/dukascopy/EURUSD_2026_01.csv",
+        index=False,
     )
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     run_ingestion()
