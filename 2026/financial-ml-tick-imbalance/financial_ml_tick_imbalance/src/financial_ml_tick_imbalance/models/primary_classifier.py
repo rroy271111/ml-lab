e7713c994935f5ca5ba6ebaad96204d7543bf839
raj <rroy271111@gmail.com>
2026-04-16 11:11:42 +0530

    fix: Fix typo in function name

--- 2026/financial-ml-tick-imbalance/financial_ml_tick_imbalance/src/financial_ml_tick_imbalance/models/primary_classifier.py
+++ 2026/financial-ml-tick-imbalance/financial_ml_tick_imbalance/src/financial_ml_tick_imbalance/models/primary_classifier.py
@@ -1,7 +1,7 @@
 from sklearn.linear_model import LogisticRegression
 
 
-def train_primary_classfier(train_df):
+def train_primary_classifier(train_df):
     X_train = train_df.drop(columns=["label"])
     y_train = train_df["label"]
 
