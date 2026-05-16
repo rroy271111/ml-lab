7bb72c118dcf60ea8f5583e618741d655f0c3867
raj <rroy271111@gmail.com>
2026-05-16 11:11:28 +0530

    Switch TFT-mini dataset from BTC to AAPL

--- 2026/temporal-fusion-transformer-mini/src/download_data.py
+++ 2026/temporal-fusion-transformer-mini/src/download_data.py
@@ -2,18 +2,39 @@ import yfinance as yf
 
 from pathlib import Path
 
-OUTPUT_PATH = Path("data/btc_usd.csv")
-
+#OUTPUT_PATH = Path("data/btc_usd.csv")
+OUTPUT_PATH = Path("data/aapl.csv")
 
 def main():
-    btc = yf.download("BTC-USD", start="2018-01-01", interval="1d")
+    #btc = yf.download("BTC-USD", start="2018-01-01", interval="1d")
 
     OUTPUT_PATH.parent.mkdir(parents=True, exist_ok=True)
 
-    btc.to_csv(OUTPUT_PATH)
+    df = yf.download(
+        "AAPL",
+        start="2018-01-01",
+        auto_adjust=True
+    )
+
+    df.columns = df.columns.get_level_values(0)
+
+    df.reset_index(inplace=True)
+    df.to_csv(OUTPUT_PATH, index=False)
+
+    print(df.head())
+    print(df.columns)
+
+    # Flatten MultiIndex columns
+    #btc.columns = btc.columns.get_level_values(0)
+
+    # Move Date index into a normal column
+    #btc.reset_index(inplace=True)
+
+    #btc.to_csv(OUTPUT_PATH)
 
     print(f"Saved data to {OUTPUT_PATH}")
-    print(btc.head())
+    #print(btc.head())
+    #print(btc.columns)
 
 
 if __name__ == "__main__":
