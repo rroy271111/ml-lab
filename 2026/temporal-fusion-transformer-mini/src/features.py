76ff134294c0367023fe43f7e08afd3e3eae27a3
raj <rroy271111@gmail.com>
2026-05-17 11:32:52 +0530

    Fix data ingestion and feature engineering pipeline

--- 2026/temporal-fusion-transformer-mini/src/features.py
+++ 2026/temporal-fusion-transformer-mini/src/features.py
@@ -1,19 +1,37 @@
 import pandas as pd
 from pathlib import Path
 
-INTPUT_PATH = Path("data/btc_usd.csv")
-OUTPUT_PATH = Path("data/btc_features.csv")
-
+#INTPUT_PATH = Path("data/btc_usd.csv")
+#OUTPUT_PATH = Path("data/btc_features.csv")
+INTPUT_PATH = Path("data/aapl.csv")
+OUTPUT_PATH = Path("data/aapl_features.csv")
 
 def add_features(df: pd.DataFrame) -> pd.DataFrame:
-    df["returns"] = df["Close"].pct_change()
+    df["returns"] = df["Close"].pct_change(fill_method=None)
 
     df["sma_7"] = df["Close"].rolling(7).mean()
 
-    df["voltality_7"] = df["returns"].rolling(7).std()
+    df["volatility_7"] = df["returns"].rolling(7).std()
 
-    df["day_of_week"] = df["Date"].date.dayofweek
+    df["day_of_week"] = df["Date"].dt.dayofweek
 
     df["month"] = df["Date"].dt.month
 
     return df
+
+def main():
+
+    df = pd.read_csv(INTPUT_PATH)
+
+    df["Date"] = pd.to_datetime(df["Date"])
+
+    df = add_features(df)   
+
+    df = df.dropna().reset_index(drop=True)
+
+    df.to_csv(OUTPUT_PATH, index=False)
+
+    print(df.head())  
+
+if __name__ == "__main__":
+    main()
