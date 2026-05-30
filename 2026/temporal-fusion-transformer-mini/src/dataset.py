d829a02818f6d8100c6615f64f6ef1649be3a234
raj <rroy271111@gmail.com>
2026-05-30 10:20:43 +0530

    refactor(data): rename dataset helper and dataloader factory

--- 2026/temporal-fusion-transformer-mini/src/dataset.py
+++ 2026/temporal-fusion-transformer-mini/src/dataset.py
@@ -1,11 +1,14 @@
 import pandas as pd
+from torch.utils.data import DataLoader
 
 from pytorch_forecasting import TimeSeriesDataSet
 
-MAX_ENCODER_LENGTH= 30
+MAX_ENCODER_LENGTH = 30
 MAX_PREDICTION_LENGTH = 7
+NUM_WORKERS = 0
+DEFAULT_BATCH_SIZE = 64
 
-def create_dataset(df:pd.DataFrame) -> TimeSeriesDataSet:
+def create_training_dataset(df: pd.DataFrame) -> TimeSeriesDataSet:
     return TimeSeriesDataSet(
         df,
         time_idx="time_idx",
@@ -40,10 +43,30 @@ def create_dataset(df:pd.DataFrame) -> TimeSeriesDataSet:
         ],
     )
 
-def create_validation_dataset(training, df):
+def create_validation_dataset(training: TimeSeriesDataSet, df: pd.DataFrame,) -> TimeSeriesDataSet:
     return TimeSeriesDataSet.from_dataset(
         training,
         df, 
         predict=True,
         stop_randomization=True,
     )
+
+
+def create_dataloaders(
+        training: TimeSeriesDataSet,
+        validation: TimeSeriesDataSet,
+        batch_size: int = 64,
+) -> tuple[DataLoader, DataLoader]:
+    train_loader = training.to_dataloader(
+        train=True,
+        batch_size=batch_size,
+        num_workers=0,
+    )
+
+    val_loader= validation.to_dataloader(
+        train=False,
+        batch_size=batch_size,
+        num_workers=0,
+    )
+
+    return train_loader, val_loader
